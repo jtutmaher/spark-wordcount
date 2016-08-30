7d65cc41ca3935853407aabd5cde80cd05d1258c
Jake Tutmaher <Tutmaher_Jacob@bah.com>
2016-08-30 16:41:17 -0400

    Added Big.txt application wordcount 16:41

---
+++ apachespark/src/main/scala/org/bah/apachespark/BigCount.scala
@@ -0,0 +1,22 @@
+package org.bah.apachespark
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object BigCount {
+  def main(args:Array[String])={
+    //Set Conf and Context for Spark
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local")
+    val sc = new SparkContext(conf)
+    
+    //Pull Lines from Big.txt
+    val lines = sc.textFile("big.txt")
+    
+    //Pull Words from lines
+    val newlines = lines.flatMap(lines=>lines.split(" "))
+    val counts = newlines.map(word=>(word,1)).reduceByKey(_+_)
+    
+    //Save File
+    counts.saveAsTextFile("BigCountSol.txt")
+  }
+  
+}
